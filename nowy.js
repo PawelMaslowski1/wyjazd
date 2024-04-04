270dce53a8df525eb550f526c5d9ac28855f701c
PawelMaslowski1 <127610603+PawelMaslowski1@users.noreply.github.com>
2024-04-04 19:18:24 +0200

    Add files via upload

--- nowy.js
+++ nowy.js
@@ -124,7 +124,7 @@ function odliczanieDoWyjazdu () {
   
   console.log(sekundy);
   
-  odliczanie.textContent = `${dni} dni, ${godziny}:${minuty};${sekundy}`
+  odliczanie.textContent = `${dni} dni ${godziny} godziny ${minuty} minut ${sekundy} sekund`
 
 }
 
